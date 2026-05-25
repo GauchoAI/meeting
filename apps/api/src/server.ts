bf67eb7010c7510b8f23efb447febc96862af8d7
Miguel Lemos <miguelemosreverte@gmail.com>
2026-05-25 03:51:00 -0300

    Aggressively tighten speech turn timing

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -438,8 +438,8 @@ async function createRealtimeCall(sdp: string, res: ServerResponse): Promise<voi
           type: "server_vad",
           create_response: true,
           interrupt_response: true,
-          prefix_padding_ms: 140,
-          silence_duration_ms: 500
+          prefix_padding_ms: 100,
+          silence_duration_ms: 420
         }
       },
       output: {
